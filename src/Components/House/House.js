bbb5bb1b8156083e419d2ec80326ed126fe19c39
Theresa Stocking <nottodayjose@MacBook-Pro.local>
2019-04-19 10:06:43 -0500

    Server Set Up

---
+++ src/Components/House/House.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class House extends Component {
+  render() {
+    return (
+      <div className="House">
+        House
+      </div>
+    );
+  }
+}
+
+export default House;
